d52391a71b505caf2bcbe82c8f64fda36c90cce6
Jean-Luc Stevens <jeanlucstevens@ymail.com>
2023-07-09 12:24:26 +1000

    add 10 buttons

--- USB_DEVICE/App/usbd_custom_hid_if.c
+++ USB_DEVICE/App/usbd_custom_hid_if.c
@@ -96,7 +96,7 @@ __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DES
 
 		/* 6 */
 		0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
-		0x09, 0x05,        // Usage (Game Pad)
+		0x09, 0x05,        // Usage (Game Pad = 0x05)
 		0xA1, 0x01,        // Collection (Application)
 
 		/* 30 */
@@ -134,11 +134,11 @@ __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DES
 		/* 16 */
 		0x05, 0x09,        //   Usage Page (Button)
 		0x19, 0x01,        //   Usage Minimum (0x01)
-		0x29, 0x08,        //   Usage Maximum (0x08)
-		0x95, 0x08,        //   Report Count (8)
+		0x29, 0x0A,        //   Usage Maximum (0x0A)
+		0x95, 0x0A,        //   Report Count (10)
 		0x75, 0x01,        //   Report Size (1)
 		0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-		0x95, 0x08,        //   Report Count (8)
+		0x95, 0x0A,        //   Report Count (10)
 		0x81, 0x03,
 
 
